cdcab9d5134a3196d72908411b66454eb8a1b5a7
j.perrochaud <j.perrochaud@student.isartdigital.com>
2023-12-15 13:41:47 +0100

    Fire update

--- Assets/Scripts/PhysicSimulation.cs
+++ Assets/Scripts/PhysicSimulation.cs
@@ -1,13 +1,58 @@
+using UnityEditor;
 using UnityEngine;
 
 public class PhysicSimulation : MonoBehaviour
 {
+    private UIManager UIref;
+
+    [Header("Assets")]
+    [SerializeField] private SpriteRenderer FireTruck = null;
+
+    [SerializeField] private SpriteRenderer Human = null;
+
+    [SerializeField] private SpriteRenderer House = null;
+
+    [Header("Variables")]
+    [SerializeField] private int EndOfSimulationOnX;
+
+    private Vector3 FireTruckAtStart; // Start pos is set in Editor
+
     private void Start()
     {
+        if (!FireTruck || !Human || !House || EndOfSimulationOnX == 0)
+        {
+            Debug.LogError("One or multiple field unset in PhysicSimulation");
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#endif
+            Application.Quit();
+        }
+
+        UIref = UIManager.Instance;
+        FireTruckAtStart = FireTruck.transform.position;
     }
 
     private void Update()
     {
-        Debug.Log(UIManager.Instance.Frequence);
+        if (UIref.IsStarted && !UIref.IsPaused)
+        {
+            Movements();
+
+            if (FireTruck.transform.localPosition.x >= EndOfSimulationOnX)
+            {
+                UIref.Reset();
+                Reset();
+            }
+        }
+    }
+
+    private void Movements()
+    {
+        FireTruck.transform.position += new Vector3(Time.deltaTime * UIref.TruckSpeed, 0f, 0f);
+    }
+
+    public void Reset()
+    {
+        FireTruck.transform.position = FireTruckAtStart;
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
@@ -14,7 +15,13 @@ public class UIManager : MonoBehaviour
         get
         {
             if (_instance == null)
+            {
                 Debug.LogError("UIManager is Null");
+#if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+#endif
+                Application.Quit();
+            }
             return _instance;
         }
     }
@@ -24,75 +31,143 @@ public class UIManager : MonoBehaviour
         _instance = this;
     }
 
-    [Header("Frequence")]
-    [SerializeField] private TextMeshProUGUI FrequenceText = null;
+    [Header("InputFields")]
+    [SerializeField] private TMP_InputField FrequenceInputField = null;
 
-    [Header("Truck Speed")]
-    [SerializeField] private TextMeshProUGUI TruckSpeedText = null;
+    [SerializeField] private TMP_InputField TruckSpeedInputField = null;
 
-    [Header("Impedence")]
-    [SerializeField] private TextMeshProUGUI ImpedenceText = null;
+    [SerializeField] private TMP_InputField ImpedenceInputField = null;
 
-    [Header("Power")]
-    [SerializeField] private TextMeshProUGUI PowerText = null;
+    [SerializeField] private TMP_InputField PowerInputField = null;
 
-    [Header("Start")]
-    [SerializeField] private Button GoButton;
+    [Header("Buttons")]
+    [SerializeField] private Button StartButton;
+
+    [SerializeField] private Button PauseButton;
+
+    [SerializeField] private Button ResetButton;
+
+    private bool _start = false;
+    private bool _paused = false;
 
     private void Start()
     {
-        if (!FrequenceText || !TruckSpeedText || !ImpedenceText || !PowerText || !GoButton)
+        if (!FrequenceInputField || !TruckSpeedInputField || !ImpedenceInputField || !PowerInputField ||
+            !StartButton || !PauseButton || !ResetButton)
         {
-            ErrorDetected("One or multiple field unset in UIManager");
-        }
-
-        Button btn = GoButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
-    }
-
-    private void ErrorDetected(string _error)
-    {
-        Debug.LogError(_error);
+            Debug.LogError("One or multiple field unset in UIManager");
 #if UNITY_EDITOR
-        EditorApplication.ExitPlaymode();
+            EditorApplication.ExitPlaymode();
 #endif
-        Application.Quit();
+            Application.Quit();
+        }
+
+        StartButton.GetComponent<Button>().onClick.AddListener(OnStartTask);
+        PauseButton.GetComponent<Button>().onClick.AddListener(OnPauseTask);
+        ResetButton.GetComponent<Button>().onClick.AddListener(OnResetTask);
+
+        PauseButton.gameObject.SetActive(false);
+        ResetButton.gameObject.SetActive(false);
     }
 
-    private void TaskOnClick()
+    private void OnStartTask()
     {
-        Debug.Log("You have clicked the go button!");
-    }
-
-    public double Frequence
-    {
-        get
+        if (FrequenceInputField.text.Length == 0 || TruckSpeedInputField.text.Length == 0 ||
+            ImpedenceInputField.text.Length == 0 || PowerInputField.text.Length == 0)
         {
-            return 0f;
+            Debug.Log("Cannot start with values empty");
+            return;
+        }
+
+        _start = true;
+
+        SwitchButtons();
+
+        FrequenceInputField.interactable = TruckSpeedInputField.interactable =
+            ImpedenceInputField.interactable = PowerInputField.interactable = false;
+    }
+
+    private void OnPauseTask()
+    {
+        if (!_paused)
+        {
+            _paused = true;
+            PauseButton.transform.Find("Text (TMP)").GetComponentInChildren<TMP_Text>().text = "Unpause";
+        }
+        else
+        {
+            _paused = false;
+            PauseButton.transform.Find("Text (TMP)").GetComponentInChildren<TMP_Text>().text = "Pause";
         }
     }
 
-    public double TruckSpeed
+    private void OnResetTask()
+    {
+        FindFirstObjectByType<PhysicSimulation>().Reset();
+        Reset();
+    }
+
+    public void Reset()
+    {
+        _start = _paused = false;
+        PauseButton.transform.Find("Text (TMP)").GetComponentInChildren<TMP_Text>().text = "Pause";
+
+        SwitchButtons();
+
+        FrequenceInputField.interactable = TruckSpeedInputField.interactable =
+            ImpedenceInputField.interactable = PowerInputField.interactable = true;
+    }
+
+    private void SwitchButtons()
+    {
+        StartButton.gameObject.SetActive(!StartButton.gameObject.activeSelf);
+        PauseButton.gameObject.SetActive(!PauseButton.gameObject.activeSelf);
+        ResetButton.gameObject.SetActive(!ResetButton.gameObject.activeSelf);
+    }
+
+    public float Frequence
     {
         get
         {
-            return 0f;
+            string input = FrequenceInputField.text.ToString();
+            return (float)(input.Length > 0 ? Convert.ToDouble(input) : 0);
         }
     }
 
-    public double Impedence
+    public float TruckSpeed
     {
         get
         {
-            return 0f;
+            string input = TruckSpeedInputField.text.ToString();
+            return (float)(input.Length > 0 ? Convert.ToDouble(input) : 0);
         }
     }
 
-    public double Power
+    public float Impedence
     {
         get
         {
-            return 0f;
+            string input = ImpedenceInputField.text.ToString();
+            return (float)(input.Length > 0 ? Convert.ToDouble(input) : 0);
         }
+    }
+
+    public float Power
+    {
+        get
+        {
+            string input = PowerInputField.text.ToString();
+            return (float)(input.Length > 0 ? Convert.ToDouble(input) : 0);
+        }
+    }
+
+    public bool IsStarted
+    {
+        get => _start;
+    }
+
+    public bool IsPaused
+    {
+        get => _paused;
     }
 }
